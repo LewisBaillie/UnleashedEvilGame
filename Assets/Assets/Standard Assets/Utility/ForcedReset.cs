d869525e999551713f98483a59a319693a94a053
Lewis Baillie <lewisbailli3@gmail.com>
2021-02-04 21:58:31 +0000

    Started Level Design
    Started level design and added wall and doorway prefabs

---
+++ Assets/Assets/Standard Assets/Utility/ForcedReset.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof (Image))]
+public class ForcedReset : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+}
