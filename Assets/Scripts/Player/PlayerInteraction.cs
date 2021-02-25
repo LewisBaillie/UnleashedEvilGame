8313d0275f9039e2ea8a46019e5b26fa76011ea2
Thomas-Hodgetts <56154847+Thomas-Hodgetts@users.noreply.github.com>
2021-02-25 17:06:45 +0000

    Re adding player items as significant interaction overhaul
    Added:
    - Old player scripts
    - A bool toggle on whether to apply gravity.
    
    Modified:
    -The way the player grabs other game objects. It no longer completely relies on triggers.
    
    Notes:
    The player toggle doesn't turn off rigid body gravity so make sure to disable it when using the player prefab

---
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_Player;
+    private ConcurrentBag<GameObject> m_ColliderCache;
+    [SerializeField]
+    private Text m_PickUpText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ColliderCache = new ConcurrentBag<GameObject>();
+        m_Player = transform.parent.parent.gameObject;
+        if (!m_Player)
+        {
+            Debug.LogError("m_PickUpText is null om PlayerInteraction.cs");
+        }
+        else
+        {
+            if (m_Player.gameObject.name != "Player")
+            {
+                Debug.LogError("m_PickUpText is null om PlayerInteraction.cs");
+            }
+        }
+
+        if (!m_PickUpText)
+        {
+            Debug.LogError("m_PickUpText is null om PlayerInteraction.cs");
+        }
+        m_PickUpText.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInteractions();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Non-Interactable" || other.tag == "Player")
+        {
+            return;
+        }
+        m_PickUpText.enabled = true;
+        m_PickUpText.text = "Pick Up " + other.gameObject.name;
+        m_ColliderCache.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TryTakeOut(other.gameObject);
+        HideText();
+    }
+
+    //Trys to remove the gameObject from the gameobject cache.
+    private void TryTakeOut(GameObject gObject)
+    {
+        if (m_ColliderCache.IsEmpty)
+        {
+            return;
+        }
+        bool result = m_ColliderCache.TryTake(out gObject);
+        Debug.Log("Take out was " + result);
+    }
+
+    private void HideText()
+    {
+        if(m_ColliderCache.IsEmpty)
+        {
+            m_PickUpText.enabled = false; 
+        }
+        else
+        {
+            GameObject[] objects = m_ColliderCache.ToArray();
+            m_PickUpText.text = "Pick Up " + objects[0].name;
+        }
+    }
+
+    private void HandleInteractions()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Parallel.ForEach(m_ColliderCache, (item) => {
+                m_Player.GetComponent<Player>().AddObjectToInvent(item);
+                item.SetActive(false);
+                m_ColliderCache.TryTake(out item);
+            });
+        }
+    }
+
+}
---
+++ Assets/Scripts/Scene Objects/SceneObjects.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneObjects : MonoBehaviour
+{
+    public bool m_IsEquipable { get; protected set; } = false;
+
+}
