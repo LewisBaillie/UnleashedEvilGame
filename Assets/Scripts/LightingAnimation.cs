7b20ea1540d429f728197c8dcee9db42812e889d
Lewis Baillie <lewisbailli3@gmail.com>
2021-03-11 16:12:48 +0000

    FixedLightingChanges

---
+++ Assets/Scripts/LightingAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingAnimation : MonoBehaviour
+{
+    private Animation animator;
+    private bool animate;
+    private int rand;
+
+    private void Start()
+    {
+        animator = GetComponent<Animation>();
+        animate = true;
+    }
+
+    private void Update()
+    {
+        if(animate)
+        {
+            animate = false;
+            rand = Random.Range(1, 10);
+
+            if(rand <= 6)
+            {
+                StartCoroutine(playAnim("PointLightAnim1"));
+            }
+            else
+            {
+                StartCoroutine(playAnim("PointLightAnim2"));
+            }
+        }
+    }
+
+    IEnumerator playAnim(string animName)
+    {
+        animator.Play(animName);
+        yield return new WaitForSeconds(Random.Range(2,6));
+        animate = true;
+    }
+}
