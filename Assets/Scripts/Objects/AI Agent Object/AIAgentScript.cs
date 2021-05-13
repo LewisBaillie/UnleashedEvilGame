e7a784bbf88344e64c6a19651c19acaea901609c
Max-A-Stockton <57523664+Max-A-Stockton@users.noreply.github.com>
2021-05-13 12:57:23 +0100

    AI Agent
    AI test scene with navmesh & moving agent which follows player

--- Assets/Scripts/Objects/AI Agent Object/AIAgentScript.cs
+++ Assets/Scripts/Objects/AI Agent Object/AIAgentScript.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Navmesh : MonoBehaviour
+public class AIAgentScript : MonoBehaviour
 {
+    [SerializeField]
+    public Transform target;
+    NavMeshAgent agent;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = target.position;
     }
 }
