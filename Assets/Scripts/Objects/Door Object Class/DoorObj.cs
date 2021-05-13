b0a417bbbb9e1557c7a2afd094c15f66f253770f
Max Osman <57242759+MaxOsman@users.noreply.github.com>
2021-05-13 12:04:44 +0100

    doors and keys
    doors and keys

---
+++ Assets/Scripts/Objects/Door Object Class/DoorObj.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorObj : Obj
+{
+    [SerializeField]
+    string _doorName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _objType = ObjectType.DoorObj;
+        _type = "Door";
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    public bool IsDoorUnlockable(string keyName)
+    {
+        return (keyName == _doorName);
+    }
+}
