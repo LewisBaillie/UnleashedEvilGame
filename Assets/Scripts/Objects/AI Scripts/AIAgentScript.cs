376e67d5025b299c4ac034ae8493506cd92939ed
Max-A-Stockton <57523664+Max-A-Stockton@users.noreply.github.com>
2021-05-20 12:36:17 +0100

    AI Wandering & AI Function Script
    Call general AI scripts from AI Functions, update function for agent currently built for

---
+++ Assets/Scripts/Objects/AI Scripts/AIAgentScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AIAgentScript : MonoBehaviour
+{
+    [SerializeField]
+    public Transform target;
+    NavMeshAgent agent;
+    string state;
+    float timeLeft; //TIME IN SECONDS
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        state = "wander";
+        agent.GetComponent<AIFunctions>().AIWander(agent);
+        timeLeft = 6.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Time
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0.0f && state == "wander")
+        {
+            agent.GetComponent<AIFunctions>().AIWander(agent);
+            timeLeft = 6.0f;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Objects/AI Scripts/AIFunctions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AIFunctions : MonoBehaviour
+{
+    //Wander AI 
+   public void AIWander(NavMeshAgent agent)
+    {
+        Vector3 position = GetRandomPoint(agent, 25f);
+        agent.destination = position;
+    }
+
+    //Get random point on the navmesh https://answers.unity.com/questions/475066/how-to-get-a-random-point-on-navmesh.html
+    public Vector3 GetRandomPoint(NavMeshAgent agent, float walkRadius)
+    {
+        Vector3 randomPos = Random.insideUnitSphere * walkRadius;
+
+        randomPos += agent.transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomPos, out hit, walkRadius, 1);
+        Vector3 finalPosition = hit.position;
+        return finalPosition;
+    }
+    
+    public void AIChase(NavMeshAgent agent, Transform target)
+    {
+        agent.destination = target.position;
+    }
+}
