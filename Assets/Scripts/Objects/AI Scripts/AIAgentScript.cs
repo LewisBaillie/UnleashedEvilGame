f87cf3e01e105076b583b193be29e405bbbf8e99
Max-A-Stockton <57523664+Max-A-Stockton@users.noreply.github.com>
2021-05-20 14:55:32 +0100

    Duplicate rooms & code sorting

--- Assets/Scripts/Objects/AI Scripts/AIAgentScript.cs
+++ Assets/Scripts/Objects/AI Scripts/AIAgentScript.cs
@@ -10,27 +10,31 @@ public class AIAgentScript : MonoBehaviour
     public Transform target;
     NavMeshAgent agent;
     string state;
-    float timeLeft; //TIME IN SECONDS
+    float timeLeft = 5.0f; //TIME IN SECONDS, starts from 5 & counts down
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         state = "wander";
-        agent.GetComponent<AIFunctions>().AIWander(agent);
+        agent.GetComponent<AIFunctions>().AIStartWander(agent);
         timeLeft = 6.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Time
-        timeLeft -= Time.deltaTime;
-        if (timeLeft <= 0.0f && state == "wander")
+        if (state == "wander")
         {
-            agent.GetComponent<AIFunctions>().AIWander(agent);
-            timeLeft = 6.0f;
+            agent.GetComponent<AIFunctions>().AIUpdateWander(agent, timeLeft, "wander");
         }
-
+        else if (state == "chase")
+        {
+            agent.GetComponent<AIFunctions>().AIUpdateChase(agent, target);
+        }
+        else if (state == "search")
+        {
+            //Search Update Function
+        }
     }
 }
