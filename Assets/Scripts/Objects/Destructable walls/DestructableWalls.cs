3af427dadbef68f4f63d098e44ae7b4a3ed30987
Thomas-Hodgetts <56154847+Thomas-Hodgetts@users.noreply.github.com>
2021-05-20 10:46:41 +0100

    New shiz

---
+++ Assets/Scripts/Objects/Destructable walls/DestructableWalls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DestructableWalls : InteractableObj
+{
+    public void ActivateDestruction(Vector3 AIForce)
+    {
+        foreach (Rigidbody body in GetComponentsInChildren<Rigidbody>())
+        {
+            body.gameObject.transform.parent = null;
+            body.AddForce(AIForce, ForceMode.VelocityChange);
+        }
+        Destroy(this);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) ;
+        {
+            ActivateDestruction(Vector3.forward * 3);
+        }
+    }
+}
