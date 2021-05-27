6a10743798fc578ce22e7f4c3a48f3d5489b6522
Max-A-Stockton <57523664+Max-A-Stockton@users.noreply.github.com>
2021-05-27 16:52:39 +0100

    (BROKEN) Start of broken wall navmesh
    BROKEN

---
+++ Assets/Scripts/Objects/AI Scripts/AINavigationManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AINavigationManager : MonoBehaviour
+{
+    public NavMeshSurface[] surfaces;
+    public void regenerateNavmesh(float timer)
+    {
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        for (int i = 0; i < surfaces.Length; i++)
+        {
+            surfaces[i].BuildNavMesh();
+        }
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        regenerateNavmesh(0);
+    }
+}
--- Assets/Scripts/Objects/Destructable walls/DestructableWalls.cs
+++ Assets/Scripts/Objects/Destructable walls/DestructableWalls.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 public class DestructableWalls : InteractableObj
 {
+    [SerializeField]
+    private AINavigationManager _AINavigationManager;
+
     public void ActivateDestruction(Vector3 AIForce)
     {
+   
         Rigidbody[] r = GetComponentsInChildren<Rigidbody>();
         foreach (Rigidbody body in GetComponentsInChildren<Rigidbody>())
         {
@@ -14,6 +19,7 @@ public class DestructableWalls : InteractableObj
             CapsuleCollider c = body.GetComponent<CapsuleCollider>();
             c.enabled = true;
         }
+        _AINavigationManager.regenerateNavmesh(0.5f);
         Destroy(gameObject);
     }
 
