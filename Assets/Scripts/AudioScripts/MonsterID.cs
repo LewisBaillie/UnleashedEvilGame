82f37b0bf662f52c26edfc6f63a80bc718188be7
Lewis Baillie <lewisbailli3@gmail.com>
2021-06-03 03:43:20 +0100

    MonsterNoise
    Added scripts for monster noise

---
+++ Assets/Scripts/AudioScripts/MonsterID.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterID : MonoBehaviour
+{
+    [SerializeField]
+    private int monsterID;
+
+    public int returnID()
+    {
+        return monsterID;
+    }
+}
